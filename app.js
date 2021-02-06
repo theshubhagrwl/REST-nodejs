7ffd179163a0690dc7071f0e2d5feba062743502
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-02-06 23:28:18 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/", (req, res) => {
+  res.send("Hello There");
+});
+
+app.listen(4000, () => {
+  console.log("Server @4000");
+});
