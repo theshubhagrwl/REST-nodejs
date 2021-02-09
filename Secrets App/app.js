90994b606c63a64ac631bde9454d0db96456b4fa
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-02-09 22:18:34 +0530

    env file and hashing

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -17,9 +19,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "yehaimerasecretjomaikisikonahibataonga.";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, {
+//   secret: process.env.secret,
+//   encryptedFields: ["password"],
+// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -33,7 +36,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, result) => {
     if (err) {
@@ -55,7 +58,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   newUser.save((err) => {
     if (!err) {
