93f73a4f91c89468c657cfe717ec2637e6dad9c9
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-02-09 13:32:36 +0530

    encrypting password

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -11,10 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "yehaimerasecretjomaikisikonahibataonga.";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
