db1d31ed0c44a12189a4de6f8193ecb65a5e37a5
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-02-09 13:14:46 +0530

    basic authentication with pass as plain text

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -16,8 +25,39 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        if (result.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 app.listen(3000, () => {
